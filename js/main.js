3173b877d4f25fb7c12b9e612a18bab098495fb6
FredyA13 <fredy.alonzo2001@hotmail.com>
2025-04-09 15:53:03 -0600

    Se crean funciones

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+//Variable del boton Guardar
 let btnGuardar = document.getElementById("guardarName");
 
-btnGuardar.addEventListener("click", function(event){
-    
+//Funcion para guarda el nombre
+function saveName(event){
     event.preventDefault();
     let name = document.getElementById("txtName").value;
     localStorage.setItem("Name", name);
+    console.log(`Nombre ingresado: ${name}`);
+}
+
+//Evento a llamar con "click" usando el boton
+btnGuardar.addEventListener("click", saveName);
+
+
+//PRIMERA PRUEBA FUNCIONAL
+// btnGuardar.addEventListener("click", function(event){
     
-});
+//     event.preventDefault();
+//     let name = document.getElementById("txtName").value;
+//     localStorage.setItem("Name", name);
+    
+// });
--- js/welcome.js
+++ js/welcome.js
@@ -1,17 +1,32 @@
+//Variable de encabezado a cambiar segun sea el caso
 let encabezado1 = document.getElementsByTagName("h1").item(0);
+
+//Variable del boton Eliminar
 let btnEliminar = document.getElementById("eliminarName");
 
+//Funcion para eliminar el nombre
+function deleteName(event){
+    event.preventDefault();
+    //Elimina el valor de clave email
+    localStorage.removeItem("Name");
+}
+
+//Evento a llamar con "click" usando el boton
+btnEliminar.addEventListener("click", deleteName);
+
+//CONDICION PARA SABER SI EXISTE LA LLAVE EN EL "localStorage"
 if(localStorage.getItem("Name") !=null){
     let name = localStorage.getItem("Name");
     encabezado1.innerText = `Hola. ${name} bienvenido/a de nuevo`;
-    console.log(name);
-
+    console.log(`Hola. ${name} bienvenido/a de nuevo`);
 }else{
     encabezado1.innerText = `Por favor ve al index e ingresa tu nombre`;
 }//!=null
 
-btnEliminar.addEventListener("click", function(event){
-    event.preventDefault();
-    //Elimina el valor de clave email
-    localStorage.removeItem("Name");
-});
+
+//PRIMERA PRUEBA FUNCIONAL
+// btnEliminar.addEventListener("click", function(event){
+//     event.preventDefault();
+//     //Elimina el valor de clave email
+//     localStorage.removeItem("Name");
+// });
